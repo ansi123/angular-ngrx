1e8890799eb55e9f0f1fa5ab10205763fdbde9d9
ansi123 <mohitsinghalcool1998@gmail.com>
2021-05-05 11:03:50 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import {postReducer} from './reducers/post.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {StoreModule} from '@ngrx/store';
+import {simpleReducer} from './simple.reducer';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({message:simpleReducer}),
   ],
   providers: [],
   bootstrap: [AppComponent]
